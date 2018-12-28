cd5554eb232fe991b4b138e812b42da6f59f80bf
linzhiqiang554940144 <45392200+linzhiqiang554940144@users.noreply.github.com>
2018-12-29 01:32:39 +0800

    Add files via upload

---
+++ js练习/01_ex.js
@@ -0,0 +1,30 @@
+/*var s ='hello,world'
+console.log(s.indexOf('world'))
+console.log(s.slice(3,5))*/
+/*var arr=[4,5,6]
+arr[5]=10
+console.log(arr)*/
+/*var arr=[4,5,6]
+arr.push(5)
+arr.pop()
+arr.reverse()
+console.log(arr)*/
+/*var arr=[5,53,12,5,15]
+
+console.log(arr.join(' '))*/
+/*var arr ={
+    name:1,
+    age:18,
+    id:32
+    
+}
+for(var i in arr){
+    var obj=arr[i]
+    console.log(arr.name)
+}*/
+var arr = ['Apple', 'Google', 'Microsoft'];
+var i, x;
+for (i=0; i<arr.length; i++) {
+    x = arr[i];
+    console.log(x);
+}
